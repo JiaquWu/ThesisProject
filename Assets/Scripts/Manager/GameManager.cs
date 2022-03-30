4371cc4262c83a4888693864f65c98b4f86cd3a7
Wu <wujiaqu1233@gmail.com>
2022-03-30 22:48:30 +0200

    add level change function

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public enum Direction {
     NONE,
     UP,
@@ -16,5 +17,45 @@ public enum InteractionType {
 }
 
 public class GameManager : SingletonManager<GameManager> {
-    
+    private List<string> allLevels;
+    private string level_01 = "level_01";
+    private string level_02 = "level_02";
+    private string level_03 = "level_03";
+    private string level_04 = "level_04";
+    private string level_05 = "level_05";
+    private string level_06 = "level_06";
+    private string level_07 = "level_07";
+    private string level_08 = "level_08";
+
+    private void Awake() {
+        allLevels = new List<string>() {
+            level_01,
+            level_02,
+            level_03,
+            level_04,
+            level_05,
+            level_06,
+            level_07,
+            level_08,
+        };
+    }
+
+    public static void LoadLevel(bool isPrevLevel) {
+        string currentLevel = SceneManager.GetActiveScene().name;
+        if(Instance.allLevels != null && Instance.allLevels.Contains(currentLevel)) {
+            int targetLevelIndex = isPrevLevel?Instance.allLevels.IndexOf(currentLevel) - 1:Instance.allLevels.IndexOf(currentLevel) + 1;
+            SceneManager.LoadScene(targetLevelIndex);//一定不是第一个或者最后一个,没有对应按钮   
+        }
+    }
+
+    public static bool isFirstLevel() {
+        string currentLevel = SceneManager.GetActiveScene().name;
+        if(Instance.allLevels != null && Instance.allLevels[0] == currentLevel) return true;
+        return false;
+    }
+    public static bool isLastLevel() {
+        string currentLevel = SceneManager.GetActiveScene().name;
+        if(Instance.allLevels != null && Instance.allLevels[Instance.allLevels.Count-1] == currentLevel) return true;
+        return false;
+    }
 }
--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,7 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : SingletonManager<UIManager> {
-    
+    [SerializeField]
+    private GameObject leftLevelButton;
+    [SerializeField]
+    private GameObject rightLevelButton;
+
+    private void Start() {
+        if(leftLevelButton != null) {
+            leftLevelButton.SetActive(!GameManager.isFirstLevel());
+        }
+        if(rightLevelButton != null) {
+            rightLevelButton.SetActive(!GameManager.isLastLevel());
+        }
+    }
+    public void OnLeftLevelButtonClicked() {
+        GameManager.LoadLevel(true);
+    }
+    public void OnRightLevelButtonClicked() {
+        GameManager.LoadLevel(false);
+    }
 }
